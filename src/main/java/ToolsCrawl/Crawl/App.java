013d91c265067c5faaa3375bc8ed0af8afe55649
Sakaeru238 <69229293+Sakaeru238@users.noreply.github.com>
2021-06-13 10:16:38 +0700

    Initial commit

---
+++ src/main/java/ToolsCrawl/Crawl/App.java
@@ -0,0 +1,13 @@
+package ToolsCrawl.Crawl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
